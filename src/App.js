4064c7212f01156097af4f62f95f3c32e92f9022
Hannah Stahl <hstahl@athenahealth.com>
2018-09-30 23:27:59 -0500

    Removed placeholder code, changed favicon, installed react-bootstrap, created nav bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Navbar collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#brand">PaleFire Designs</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="#">
+                Shop
+              </NavItem>
+              <NavItem eventKey={2} href="#">
+                About
+              </NavItem>
+              <NavItem eventKey={3} href="#">
+                Events
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </div>
     );
   }
