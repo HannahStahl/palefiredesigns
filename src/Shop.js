0d0b1eef4aaa08226766075ff7f240c911b20ca2
Hannah Stahl <hls62@cornell.edu>
2018-11-13 16:04:28 -0600

    Restyled modal

--- src/Shop.js
+++ src/Shop.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from './Navigation';
-import { Carousel, Thumbnail, Modal, Button, Collapse } from 'react-bootstrap';
+import { Carousel, Thumbnail, Modal, Button } from 'react-bootstrap';
 
 class Shop extends Component {
   constructor() {
@@ -26,16 +26,9 @@ class Shop extends Component {
         <Navigation />
         <Modal show={this.state.showModal} onHide={this.handleCloseModal}>
           <Modal.Header className="modal-header" closeButton>
-            <Modal.Title className="h3">Lampwork Drop Pendant</Modal.Title>
-            <div>
-              <p>Once I held Sheri Fisher's beautiful lampwork glass drop... </p>
-              <a className="read-more" onClick={() => this.setState({ showText: !this.state.showText })}>Read more</a>
-              <Collapse in={this.state.showText}>
-                <div><span><p>
-                    (simply can't capture the true beauty if a photo!) in my hand and moved it around in the light, I knew I had to hang it from several strands of rainbow metallic tri-cut seed beads I'd been hoarding until just the right piece came along! I also recently acquired a tribal-looking solid bronze ring bead that also needed to be worked into the mix. My beadwoven bails of cranberry luster seed beads to gather the seed beads and connect to the ring and another, smaller bail of tiny bronze seed beads to connect the ring to the lampwork pendant pull it all together. And don't overlook the lovely textured bronze cones and the fabulous organic bronze toggle clasp by Russian metalsmith, Anna Chernykh of AnnaBronze! A nice 16-inch length, with a 2-inch pendant, including the hand beaded bails.        
-                </p></span></div>
-              </Collapse>
-            </div>
+            <Modal.Title className="h3">Lampwork Drop Pendant Multistrand Necklace with Beaded Bails, Tricut Seed Beads and Solid Bronze Disc</Modal.Title>
+            <Button className="add-to-cart button">$145 - Add to Cart</Button>
+            <br/><a className='details-link' href="#">View full details</a>
           </Modal.Header>
           <Modal.Body>
             <Carousel interval={null}>
@@ -50,10 +43,6 @@ class Shop extends Component {
               </Carousel.Item>
             </Carousel>
           </Modal.Body>
-          <Modal.Footer className="modal-footer centered">
-            <Button className="button">Add to Cart</Button>
-            <p style={{"paddingTop": "10px"}}>$145</p>
-          </Modal.Footer>
         </Modal>
           <div className='items'>
             <Thumbnail href="#" onClick={this.handleShowModal} alt="necklace" src="./necklace.jpg" className="centered">
